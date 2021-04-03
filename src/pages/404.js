48d453675177456d29d4b89a7cb06a1b285900fd
Yinka Adedire <dryinkuzz@gmail.com>
2021-04-03 15:42:11 +0100

    style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Ghost } from 'react-kawaii';
+import styled from 'styled-components';
 
-const NotFoundPage = ({ data }) => {
+const NotFoundPage = () => {
   return (
     <Layout title="404 Not Found">
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist..</p>
-      <Ghost size={240} mood="sad" color="#E0E4E8" />
+      <NotFoundPageWrapper>
+        <Ghost size={240} mood="sad" color="#E0E4E8" />
+        <h1>OOPS..</h1>
+        <p>404. Page not found</p>
+      </NotFoundPageWrapper>
     </Layout>
   );
 };
 
 export default NotFoundPage;
+
+const NotFoundPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
