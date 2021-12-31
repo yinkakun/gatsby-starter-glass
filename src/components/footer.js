5ff46574f599dfcdc034454ccc47a6e1d2b0f6ac
Moto Sakanosita <moto@sakanosita.org>
2021-12-31 18:38:45 +0900

    chore

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <StyledFooter>
       <FooterWrapper>
         <SocialLinks />
-        © 2021 Moto Sakanosita
+        © 2021 Sakanosita ICP Labs
         {/* <FooterAttribution>
           Hand-crafted with love by{' '}
           <a href="https://twitter.com/yinkakun">Yinka Adedire</a>
